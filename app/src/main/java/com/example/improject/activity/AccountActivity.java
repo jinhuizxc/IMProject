8a0dcbf13a841612c11c48d9da36d087ac70a7b7
j_h <13113017500>
2019-08-24 17:02:12 +0800

    数据库+数据model;

--- app/src/main/java/com/example/improject/activity/AccountActivity.java
+++ app/src/main/java/com/example/improject/activity/AccountActivity.java
@@ -3,7 +3,6 @@ package com.example.improject.activity;
 import android.content.Context;
 import android.content.Intent;
 
-import com.example.improject.common.app.BaseActivity;
 import com.example.improject.R;
 import com.example.improject.fragments.user.UpdateInfoFragment;
 
